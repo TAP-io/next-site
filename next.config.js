15a4ca2590ddee77909444e8759955daf5bb31e8
sam-schmitt <samuelgschmitt@gmail.com>
2022-10-08 23:17:35 -0500

    heroku config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  exportTrailingSlash: true,
+module.exports = {
+	reactStrictMode: true,
+	images: {
+		unoptimized: true,
+	},
+	async headers() {
+		return [
+			{
+				source: "/.well-known/apple-app-site-association",
+				headers: [
+					{
+						key: "Content-Type",
+						value: "application/json",
+					},
+				],
+			},
+			{
+				source: "/.well-known/assetlinks.json",
+				headers: [
+					{
+						key: "Content-Type",
+						value: "application/json",
+					},
+				],
+			},
+		];
+	},
 };
